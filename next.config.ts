0491e69a3d8eeb2ededd8c38c5781fe59a2688bf
Jeferson Gomes da Silva <jefe.gomes154@gmail.com>
2025-11-07 23:42:57 -0300

    Configuracao render

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/off-grid-system',
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
