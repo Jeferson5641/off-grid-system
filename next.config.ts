e5d67371d288f123e726c97ad9e5ba2e734d319f
Jeferson Gomes da Silva <jefe.gomes154@gmail.com>
2025-11-08 00:02:08 -0300

    create docs files

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: '/off-grid-system',
+  distDir: 'docs',
   reactCompiler: true,
   images: {
     unoptimized: true,
